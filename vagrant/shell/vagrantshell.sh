167d229eed5004562f066a8e233da4997f789877
Claudia Perez <>
2017-11-22 11:33:50 +0100

    se prepara el shell y el playbook para realizar el aprovisionamiento

---
+++ vagrant/shell/vagrantshell.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# Update Centos 6.5
+    sudo yum update -y
+    sudo yum install libselinux
+    sudo yum install libselinux-python -y
+
+
+if ! ansible --version | grep ansible;
+then
+    echo "-> Installing Ansible"
+    # Add Ansible Repository & Install Ansible
+    cd /tmp
+    # RHEL/CentOS 6.5 64-Bit ##
+    wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    wget http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
+    rpm --import /temp/RPM-GPG-KEY-EPEL-6
+    rpm -ivh /tmp/epel-release-6-8.noarch.rpm
+    yum repolist
+    # Update Centos 6.5
+    sudo yum update -y
+    
+    # Install Ansible
+    sudo yum install ansible -y
+else
+        echo "-> Ansible already Installed!"
+fi
+
+# Config Lang Centos 6
+    echo 'LANG=en_US.utf-8' > /etc/environment
+    echo 'LC_ALL=en_US.utf-8' > /etc/environment
+# Installing Ansible Galaxy
+echo "-> Installing Ansibe Galaxy Modules"
+role_list[0]='geerlingguy.java'
+role_list[1]='geerlingguy.mysql'
+role_list[2]='geerlingguy.nodejs'
+role_list[3]='geerlingguy.git'
+
+for role_and_version in "${roles_list[@]}"
+do
+    role_and_version_for_grep="${role_and_version/,/, }"
+
+    if ! sudo ansible-galaxy list | grep -qw "$role_and_version_for_grep";
+    then
+            echo "Installing ${role_and_version}"
+            sudo ansible-galaxy -f install $role_and_version
+   else
+        echo "Already installed ${role_and_version}"
+    fi
+done
+
+echo "Disable permanently SE Linux in apache"
+sudo setenforce 0;
+
+sed -i --follow-symlinks 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux && cat /etc/sysconfig/selinux
+
+# Execute Ansible
+echo "-> Execute Ansible"
+ansible-playbook /ansible/playbook.yml -i /ansible/inventories/hosts --connection=local
+
+# echo "Reset machine"
+sudo reboot;
+
